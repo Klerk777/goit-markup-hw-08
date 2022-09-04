0871e98d09379e0170ed7a7a1095c71845b1178b
Volodymyr Bakumenko <Klerk.mail@gmail.com>
2022-09-04 16:44:06 +0300

    Fixed mistakes with menu-container. Finaly finished adaptive makeup.
    Changes to be committed:
    	modified:   SASS/base/_common.scss
    	modified:   SASS/layouts/_header.scss
    	modified:   css/main.css
    	modified:   css/main.css.map
    	modified:   css/main.min.css
    	modified:   css/main.min.css.map
    	modified:   index.html
    	modified:   js/mobile-menu.js

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,15 +1,17 @@
 (() => {
-	const menuBtnRef = document.querySelector("[data-menu-button]");
-	const mobileMenuRef = document.querySelector("[data-menu]");
-	const ScrollToTop = document.querySelector("[data-scroll]");
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+  const ScrollToTop = document.querySelector("[data-scroll]");
+  const NotScrollBody = document.querySelector("[data-not-scroll-body]");
 
-	menuBtnRef.addEventListener("click", () => {
-		const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+  menuBtnRef.addEventListener("click", () => {
+    const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
 
-		menuBtnRef.classList.toggle("is-open");
-		menuBtnRef.setAttribute("aria-expanded", !expanded);
+    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
 
-		mobileMenuRef.classList.toggle("is-open");
-		ScrollToTop.classList.toggle("is-open");
-	});
+    mobileMenuRef.classList.toggle("is-open");
+    ScrollToTop.classList.toggle("is-open");
+    NotScrollBody.classList.toggle("not-scroll");
+  });
 })();
